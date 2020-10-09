b2c9fbc0ef59675e53d9259cab4ffd1c202863ac
droo303 <petrkunk@gmail.com>
2020-10-09 15:15:34 +0200

    add 10152

--- 10152 - ShellSort/main.cpp
+++ 10152 - ShellSort/main.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-struct turtle{
-    char name[8];
-    int order;
-};
-
-
 int main() {
 
-    int k;
+    int k; //count of problems
     cin >> k;
 
     for (int i = 0; i < k; ++i) {
 
-        int n;
+        int n; //height
         cin >> n;
 
+        string unsorted[n];
+        string sorted[n];
+
+        cin.ignore();
         for (int j = 0; j < n; ++j) {
-
+            getline(cin,unsorted[j]);
         }
+        for (int l = 0; l < n; ++l) {
+            getline(cin,sorted[l]);
+        }
+
+        int s = n - 1;
+        for (int m = n - 1; m >= 0; --m) {
+            if (unsorted[m] == sorted[s]) {
+                --s;
+            }
+        }
+
+        for (int i1 = s; i1 >= 0; --i1) {
+            cout << sorted[i1] << endl;
+        }
+        cout << endl;
     }
-
-
     return 0;
 }
